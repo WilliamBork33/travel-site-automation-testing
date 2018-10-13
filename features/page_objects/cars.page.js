a5663f1933dc6af7a60dace65a883ed69d364816
WilliamBork33 <williambork33@gmail.com>
2018-10-13 17:45:51 -0500

    Working on Time Selection

--- features/page_objects/cars.page.js
+++ features/page_objects/cars.page.js
@@ -7,10 +7,38 @@ class CarsPage extends Page {
     get locationBox2() { return browser.element('#s2id_carlocations > a'); }
     get dropOffLocation () { return browser.element('#select2-drop > div > input'); }
 
+    get departCalendar () { return browser.element('#departcar'); }
+    get departDate () { return browser.element('body > div:nth-child(13) > div.datepicker-days > table > tbody > tr:nth-child(3) > td:nth-child(2)'); }
+
+    get departTimeBox () { return browser.element('#body-section > div.search_head > div > form > div:nth-child(4) > div > select'); }
+    get departTime () { return browser.element('#body-section > div.search_head > div > form > div:nth-child(4) > div > select'); }
+
+
+
+    a() { 
+        var selectBox = this.departTimeBox
+        console.log(selectBox.getValue()); // returns "someValue0"
+        //selectBox.setValue('06:00') ////  HERE TO CONTINUE TRYING TO FIND OUT ABOUT Error: An element command could not be completed because the element is in an invalid state (e.g. attempting to click a disabled element).at elementIdValue
+
+        //selectBox.selectByValue('someValue3');
+        // console.log(selectBox.getValue()); // returns "someValue3"
+    
+        //var selectBox = this.departTimeBox
+        //console.log(selectBox.getText('option:checked')); // returns "uno"
+    
+        //selectBox.selectByVisibleText('cuatro');
+        //console.log(selectBox.getText('option:checked')); // returns "cuatro"
+
+    }
+
     carsURL() {
         browser.url('http://www.phptravels.net/cars');
     }
     
+    carsConfirm() {
+        browser.click('#select2-drop > ul > li > div');
+    }
+
 
 }
  
--- features/steps/cars.steps.js
+++ features/steps/cars.steps.js
@@ -9,7 +9,8 @@ module.exports =  function() {
         // Write code here that turns the phrase above into concrete actions
         carsPage.carsURL();
         carsPage.locationBox.click()
-        carsPage.pickUpLocation.setValue('cairo');
+        carsPage.pickUpLocation.setValue('Cairo');
+        carsPage.carsConfirm();
         //carsPage.locationBox2.click()
         //carsPage.dropOffLocation.setValue('alexandria');
       });
@@ -17,6 +18,21 @@ module.exports =  function() {
     this.When(/^the desired filters are applied$/, function () {
         // Write code here that turns the phrase above into concrete actions
         console.log("aasdf")
+
+
+        carsPage.departCalendar.click();
+        carsPage.departDate.click()
+
+        carsPage.departTimeBox.click()
+
+
+        carsPage.a();
+        //carsPage.departTimeBox.setValue('09:00')
+        //carsPage.tabBar.click()
+        //carsPage.tabBar.setValue()
+        //carsPage.tabBar2.click()
+        //carsPage.pickUpLocation.setValue('cairo');
+
       });
 
     this.Then(/^the desired cars are displayed$/, function () {
